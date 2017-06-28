25b237d85a775e61eaa9d38f4910ba9923ddff13
Jason Cannavale <jason@cannavale.com>
2017-06-28 17:03:57 +0100

    add some conditions to keep originals

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,21 @@
 #!/bin/bash
-ln -s ~/.dotfiles/vimrc ~/.vimrc
+if [ -f ~/.bashrc ]; then
+    mv ~/.bashrc ~/.bashrc.orig
+fi
+
+if [ -f ~/.bash_profile ]; then
+    mv ~/.bash_profile ~/.bash_profile.orig
+fi
+
+if [ -f ~/.tmux.conf ]; then
+    mv ~/.tmux.conf ~/.tmux.conf.orig
+fi
+
+if [ -f ~/.vimrc ]; then
+    mv ~/.vimrc ~/.vimrc.orig
+fi
+
 ln -s ~/.dotfiles/bashrc ~/.bashrc
 ln -s ~/.dotfiles/bash_profile ~/.bash_profile
 ln -s ~/.dotfiles/tmux.conf ~/.tmux.conf
+ln -s ~/.dotfiles/vimrc ~/.vimrc
